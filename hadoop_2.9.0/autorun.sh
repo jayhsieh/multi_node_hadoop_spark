f35f089e508b103ede00ac2e48f998850a50760b
Ubuntu <ubuntu@master.c.nccu-distribution-system.internal>
2017-12-18 03:44:30 +0000

    add script

--- hadoop_2.9.0/autorun.sh
+++ hadoop_2.9.0/autorun.sh
@@ -1,3 +1,5 @@
+sudo su -c "passwd root; passwd jay_cc_hsieh; passwd ubuntu;"
+cat user_env >> ~/.bashrc
 wget http://apache.stu.edu.tw/hadoop/common/hadoop-2.9.0/hadoop-2.9.0.tar.gz
 tar -zxvf hadoop-2.9.0.tar.gz
 
